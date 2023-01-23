e1a42cd1c126990966eb2ca92b0bcfc3c0422324
DicksonWachai <wachaidickson@gmail.com>
2023-01-23 12:14:46 +0300

    Changes to code

--- ex_1.py
+++ ex_1.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
 """Given two integer numbers return their product only if the product is equal to or lower than 1000, else return their sum."""
-def multi_sum(number1, number2):
-    # user inputs number 1 and number 2
-    number1 = input("Enter number1: ")
-    number2 = input("Enter number2: ")
-    product = number1 * number2
-    sum = number1 + number2
-    if product <= 1000:
-        return product
-    else:
-        return sum
+# user inputs number 1 and number 2
+number1 = input("Enter number1: ")
+number2 = input("Enter number2: ")
+product = int(number1) * int(number2)
+sum = int(number1) + int(number2)
+if product <= 1000:
+    print(product)
+else:
+    print(sum)
